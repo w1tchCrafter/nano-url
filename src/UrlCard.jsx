5a211ef155d96f035b0d4281d8c936c369ca4b0d
w1tchCrafter <guilhermenunes32434@outlook.com>
2024-05-01 23:54:42 -0300

    added link copying

--- src/UrlCard.jsx
+++ src/UrlCard.jsx
@@ -4,6 +4,10 @@ import { faTrash, faCopy } from "@fortawesome/free-solid-svg-icons";
 export default function UrlCard(props) {
   const { original, link } = props;
 
+  const copy = async () => {
+    await navigator.clipboard.writeText(link);
+  };
+
   return (
     <li className="url-container">
       <div className="url-texts">
@@ -11,7 +15,7 @@ export default function UrlCard(props) {
         <a href={original}>{original}</a>
       </div>
       <div className="url-actions">
-        <FontAwesomeIcon onClick={navigator.clipboard.writeText(link)} icon={faCopy} />
+        <FontAwesomeIcon onClick={copy} icon={faCopy} />
         <FontAwesomeIcon icon={faTrash} />
       </div>
     </li>
